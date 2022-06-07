8cf8b4f0a30341a4a591fc85bfc30b67f9c846d1
DPascuttini <dominik.pascuttini1@gmail.com>
2022-06-07 17:15:07 +0200

    Updated react-router-dom & react-router-bootstrap

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,18 +6,18 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <LinkContainer to='/'>
+          <LinkContainer to={`/`}>
             <Navbar.Brand>PaskiShop</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <LinkContainer to='/cart'>
+              <LinkContainer to={`/cart`}>
                 <Nav.Link>
                   <i className='fas fa-shopping-cart'></i> Cart
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/login'>
+              <LinkContainer to={`/login`}>
                 <Nav.Link>
                   <i className='fas fa-user'></i>Sign In
                 </Nav.Link>
