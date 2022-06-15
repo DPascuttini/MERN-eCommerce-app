45e0886b1a4b814e4d613d030700b254aacd1a87
DPascuttini <dominik.pascuttini1@gmail.com>
2022-06-15 17:20:33 +0200

    updated syntax

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -17,8 +17,8 @@ import { listProductDetails } from '../actions/productActions';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 
-const ProductScreen = ({ history, match }) => {
-  const [qty, setQty] = useState(0);
+const ProductScreen = ({ match }) => {
+  const [qty, setQty] = useState(1);
 
   const dispatch = useDispatch();
   const { id } = useParams();
