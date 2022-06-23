d0d202eb9a2243ef1dc499adc104aa80a401dd16
DPascuttini <dominik.pascuttini1@gmail.com>
2022-06-23 18:28:02 +0200

    set up custom auth middleware

--- backend/data/users.js
+++ backend/data/users.js
@@ -5,7 +5,7 @@ const users = [
     name: 'Admin User',
     email: 'admin@example.com',
     password: bcrypt.hashSync('123456', 10),
-    isAdming: true,
+    isAdmin: true,
   },
   {
     name: 'Marko',
