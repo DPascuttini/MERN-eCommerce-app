a74ad9dd66962f47487b3dda7259616266df8064
DPascuttini <dominik.pascuttini1@gmail.com>
2022-07-12 01:30:36 +0200

    added check for order

--- frontend/src/screens/orderScreen.js
+++ frontend/src/screens/orderScreen.js
@@ -28,8 +28,10 @@ const OrderScreen = () => {
   }
 
   useEffect(() => {
-    dispatch(getOrderDetails(orderId));
-  }, [dispatch, orderId]);
+    if (!order || order._id !== orderId) {
+      dispatch(getOrderDetails(orderId));
+    }
+  }, [order, orderId, dispatch]);
 
   return loading ? (
     <Loader />
